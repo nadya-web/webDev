e8b3dde45f4a4ee1c3e3a462d8d8d6c01d4625ef
Nadya <65254064+nadya-web@users.noreply.github.com>
2021-04-07 21:34:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,109 @@
-window.onload=function(){
-	let subArrow = document.querySelector('#subArr');
-	let monArrow = document.querySelector('#monArr');
-	let hiddenSub = document.querySelectorAll('.hiddenSub');
-	let hiddenMon = document.querySelectorAll('.hiddenMon');
-	let subject = document.querySelector('#subject');
-	let month = document.querySelector('#month');
-	let counter1 = 0;
-	let counter2 = 0;
-	let flag = 0;
-	let choosenSub = '';
-	let choosenMon = '';
+(function(){
+    
+    var canvas = document.createElement('canvas'),
+    context = canvas.getContext('2d'),
+    w = canvas.width = innerWidth,
+    h = canvas.height = innerHeight,
+    particles = [],
+    properties = {
+      bgColor: 'rgba(17, 17, 18, 1)',
+      particleColor: 'rgba(229, 36, 255, 1)',
+      particleRad: 3,
+      particleCount: 60,
+      particleMaxVelocity: 0.5,
+      lineLen: 150,
+      particleLife: 6
+    };
+    
+    document.querySelector('body').appendChild(canvas)
 
-	subArrow.addEventListener('click', function(){
-		counter1++;
-		for(let i=0; i < hiddenSub.length; i++){
-			if (hiddenSub[i].classList.contains('hideActive')){
-				hiddenSub[i].classList.remove('hideActive');
-			}
-			else {
-				hiddenSub[i].classList.add('hideActive');
-			}
-		}
-		rotate(subArrow, counter1);
-	})
-	monArrow.addEventListener('click', function(){
-		counter2++;
-		for(let i=0; i < hiddenMon.length; i++){
-			if (hiddenMon[i].classList.contains('hideActive')){
-				hiddenMon[i].classList.remove('hideActive');
-			}
-			else {
-				hiddenMon[i].classList.add('hideActive');
-			}
-		}
-		rotate(monArrow, counter2);
-	})
-	function rotate(arrow, counter){
-		if (counter%2 == 1){	
-			arrow.style.transform = 'rotate(+135deg)';
-		}
-		else {
-			arrow.style.transform = 'rotate(-45deg)'
-		}
-	}
-	for (let i=0; i < hiddenSub.length; i++){
-		hiddenSub[i].addEventListener('click', function(){
-			choosenSub = hiddenSub[i].innerHTML;
-			flag++;
-			subject.innerHTML = choosenSub;
-			for(let j=0; j<hiddenSub.length; j++){
-				hiddenSub[j].classList.remove('hideActive');
-			}
-			send(choosenSub, choosenMon, flag);
-			counter1++;
-			rotate(subArrow, counter1);
-		})
-	}
-	for (let i=0; i < hiddenMon.length; i++){
-		hiddenMon[i].addEventListener('click', function(){
-			choosenMon = hiddenMon[i].innerHTML;
-			flag++;
-			month.innerHTML = choosenMon;
-			for(let j=0; j<hiddenMon.length; j++){
-				hiddenMon[j].classList.remove('hideActive');
-			}
-			send(choosenSub, choosenMon, flag);
-			counter2++;
-			rotate(monArrow, counter2);
-		})
-	}
-	function send(choosenSub, choosenMon, flag){
-		if (flag >= 2){
-			$.ajax({
-				url: 'index.php',
-				type: "POST",
-				dataType: "json",
-				data: {mon: choosenMon, sub: choosenSub},
-				success: function(response){
-					alert(response.status);
-				},
-				error: function(response){
-					alert(response.status, "err");
-				}
-			});
-		}
-	}
-}
+    window.onresize = function(){
+        w = canvas.width = innerWidth,
+        h = canvas.height = innerHeight;
+    }
+
+    class Particle{
+        constructor(){
+            this.x = Math.random()*w;
+            this.y = Math.random()*h;
+            this.velocityX = Math.random()*(properties.particleMaxVelocity*2)-properties.particleMaxVelocity;
+            this.velocityy = Math.random()*(properties.particleMaxVelocity*2)-properties.particleMaxVelocity;
+            this.life = Math.random()*properties.particleLife*60;
+        }
+        position(){
+            this.x += this.velocityX;
+            this.Y += this.velocityY;
+            this.x + this.velocityX > w && this.velocityX > 0 || this.x + this.velocityX > w && this.velocityX < 0? this.velocityX *= -1 : this.velocityX;
+            this.y + this.velocityY > h && this.velocityY > 0 || this.y + this.velocityY > h && this.velocityY < 0? this.velocityY *= -1 : this.velocityY;
+        }
+        reDraw(){
+            context.beginPath();
+            context.arc(this.x, this.y, properties.particleRad, 0, Math.PI*2)
+            context.closePath();
+            context.fillStyle = properties.particleColor;
+            context.fill();
+        }
+        reCalculateLife(){
+            if(this.life < 1){
+                this.x = Math.random()*w;
+                this.y = Math.random()*h;
+                this.velocityX = Math.random()*(properties.particleMaxVelocity*2)-properties.particleMaxVelocity;
+                this.velocityy = Math.random()*(properties.particleMaxVelocity*2)-properties.particleMaxVelocity;
+                this.life = Math.random()*properties.particleLife*60;
+            }
+            this.life--;
+        }
+    }
+
+    function reDrawBackground(){
+        context.fillStyle = properties.bgColor;
+        context.fillRect(0,0,w,h);
+    };
+
+    function drawLines(){
+        var x1, y1, x2, y2, length, opacity;
+        for(var i in particles){
+            for(var j in particles){
+                x1 = particles[i].x;
+                y1 = particles[i].y;
+                x2 = particles[j].x;
+                y2 = particles[j].y;
+                length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+                if (length < properties.lineLen){
+                    opacity = 1 - length/properties.lineLen;
+                    context.lineWidth = '0,5';
+                    context.strokeStyle = 'rgba(229, 36, 255, '+opacity+')';
+                    context.beginPath();
+                    context.moveTo(x1,y1);
+                    context.lineTo(x2,y2);
+                    context.closePath;
+                    context.stroke();
+                }
+            }
+        }
+    }
+
+    function reDrawParticles(){
+        for(var i in particles){
+            particles[i].reCalculateLife();
+            particles[i].position();
+            particles[i].reDraw();
+        }
+    }
+
+    function loop(){
+        reDrawBackground();
+        reDrawParticles();
+        drawLines();
+        requestAnimationFrame(loop);
+    };
+
+    function init(){
+        for(var i=0; i < properties.particleCount; i++){
+            particles.push(new Particle);
+        }
+        loop();
+    };
+
+    init();
+}())
